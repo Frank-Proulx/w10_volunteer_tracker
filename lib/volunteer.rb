7f774c493ad858191f83aea90755a9280a8a5f82
Frank-Proulx <frankproulx7@gmail.com>
2021-12-24 11:51:20 -0800

    add initialize method to Volunteer having passed both tests

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -1,1 +1,12 @@
 require('pry')
+
+class Volunteer
+  attr_accessor :name, :project_id, :id
+  
+  def initialize(attributes)
+    @name = attributes.fetch(:name)
+    @project_id = attributes.fetch(:project_id)
+    @id = attributes.fetch(:id)
+  end
+
+end
